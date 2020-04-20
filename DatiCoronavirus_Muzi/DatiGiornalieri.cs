02aa18fbf633a30c609a1d4fcbd06ec154584adf
Filippo <f.izum02@gmail.com>
2020-04-20 17:10:27 +0200

    perfemette di visualizzare i dati giorno per giorno sul covid-19

---
+++ DatiCoronavirus_Muzi/DatiGiornalieri.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatiCoronavirus_Muzi
+{
+    class DatiGiornalieri
+    {
+        public DateTime Data { get; set; }
+        public int TotPositivi { get; set; }
+        public int NuoviPositivi { get; set; }
+        public int DimessiGuariti { get; set; }
+        public int Deceduti { get; set; }
+        public int TotCasi { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Data.ToShortDateString()}: positivi:{TotPositivi}, nuovi:{NuoviPositivi}, dimessi:{DimessiGuariti}, deceduti:{Deceduti}, casi totali:{TotCasi}";
+        }
+    }
+}
--- DatiCoronavirus_Muzi/MainWindow.xaml.cs
+++ DatiCoronavirus_Muzi/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace DatiCoronavirus_Muzi
 {
@@ -23,6 +25,39 @@ namespace DatiCoronavirus_Muzi
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CaricaDati()
+        {
+            string path = @"dati_covid19.xml";
+            XDocument xmlDoc = XDocument.Load(path);
+            XElement xmlVirus = xmlDoc.Element("coronavirus");
+            var xmlGiornata = xmlVirus.Elements("giornata");
+            foreach (var item in xmlGiornata)
+            {
+                XElement xmlData = item.Element("data");
+                XElement xmlPositivi = item.Element("totale_positivi");
+                XElement xmlNuoviPos = item.Element("nuovi_positivi");
+                XElement xmlDimessi = item.Element("dimessi_guariti");
+                XElement xmlDeceduti = item.Element("deceduti");
+                XElement xmlTotCasi = item.Element("totale_casi");
+
+                DatiGiornalieri dg = new DatiGiornalieri();
+                dg.Data = Convert.ToDateTime(xmlData.Value);
+                dg.TotPositivi = Convert.ToInt32(xmlPositivi.Value);
+                dg.NuoviPositivi = Convert.ToInt32(xmlNuoviPos.Value);
+                dg.DimessiGuariti = Convert.ToInt32(xmlDimessi.Value);
+                dg.Deceduti = Convert.ToInt32(xmlDimessi.Value);
+                dg.TotCasi = Convert.ToInt32(xmlTotCasi.Value);
+                Dispatcher.Invoke(() => Lst_Lista.Items.Add(dg));
+                Thread.Sleep(50);
+            }
+        }
+
+        private void Btn_Visualizza_Click(object sender, RoutedEventArgs e)
+        {
+            Lst_Lista.Items.Clear();
+            Task.Factory.StartNew(() => CaricaDati());
         }
     }
 }
